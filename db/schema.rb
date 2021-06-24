539608155b0d89a1ab94c0b0dcfff6499cada3a9
panna001 <sonar.pocket.0926@gmail.com>
2021-06-24 05:37:44 +0000

    [Add]userinfo

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,14 @@ ActiveRecord::Schema.define(version: 2021_06_24_045344) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "profile_image_id"
+    t.string "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
