9fed29d75ebf0b470de95651bcdb36e18c870e28
panna001 <sonar.pocket.0926@gmail.com>
2021-06-24 04:25:46 +0000

    [add] signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :config_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def config_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys[:email])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_06_24_024837) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
