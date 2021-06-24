2876524d11f50d537fbf495c9a0f64e93713b96f
panna001 <sonar.pocket.0926@gmail.com>
2021-06-24 04:51:58 +0000

    [Add]refile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :books, only:[:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only:[:index, :show, :edit, :update, :destroy]
   root 'homes#top'
   get 'homes/about'
   devise_for :users
