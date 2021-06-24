a83fe23b8c9efbfbac063717362afd6f158d2d43
panna001 <sonar.pocket.0926@gmail.com>
2021-06-24 05:16:38 +0000

    [Add]session

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_024837) do
+ActiveRecord::Schema.define(version: 2021_06_24_045344) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
